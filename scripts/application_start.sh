a8383c6f4714da9c8deb5b3199a2b89d28f72232
Lahiru Wijesinghe <lahiruw@conceptvines.com>
2022-08-09 15:51:08 +0530

    crlf to lf

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -5,5 +5,5 @@ git clone https://github.com/lahiruw-cv/auto-scaling-nodejs-app
 cd auto-scaling-nodejs-app
 chmod -R 755 .
 npm install
-pm2 start app.js --name "my-app" -i max
+pm2 start app.js --name "my-app" -i 3
 pm2 list
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ex
 su - ec2-user
 cd /home/ec2-user/
-pm2 stop my-app
+pm2  stop my-app
 rm -r auto-scaling-nodejs-app
